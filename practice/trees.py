4376b929410ce1951a52d73f6b022373a62d1a5b
Aryan Jain <aryanjn807@gmail.com>
2023-05-09 06:21:48 -0400

    added more on tree traversal

--- practice/trees.py
+++ practice/trees.py
@@ -14,9 +14,40 @@ class Node:
 
 
 class BST:
-    def __init__(self) -> None:
+    def __init__(self, val) -> None:
+        self.root = Node(val)
 
-        self.root = None
+    def preorder_t(self, current):
+
+        if not current:
+            return
+
+        # visit left right
+        print(current.val, ", ", end="")
+        self.preorder_t(current.left)
+        self.preorder_t(current.right)
+
+    # Ascending Order.
+    def inorder_t(self, current):
+
+        if not current:
+            return
+
+        #  left visit right
+        self.inorder_t(current.left)
+        print(current.val, ", ", end="")
+        self.inorder_t(current.right)
+
+    # Descending Order.
+    def postorder_t(self, current):
+
+        if not current:
+            return
+
+        #  right visit left 
+        self.postorder_t(current.right)
+        print(current.val, ", ", end="")
+        self.postorder_t(current.left)
 
     def add(self, current, val):
 
@@ -27,7 +58,7 @@ class BST:
             if current.val < val:
                 if not current.right:
                     current.right = Node(val)
-                else:                
+                else:
                     self.add(current.right, val)
             else:
                 if not current.left:
@@ -36,5 +67,17 @@ class BST:
                     self.add(current.left, val)
 
 
+bst = BST(7)
+root = bst.root
 
+bst.add(root, 5)
+bst.add(root, 3)
+bst.add(root, 6)
+bst.add(root, 4)
+bst.add(root, 12)
+bst.add(root, 8)
+bst.add(root, 13)
 
+# bst.preorder_t(root)
+# bst.inorder_t(root)
+bst.postorder_t(root)
