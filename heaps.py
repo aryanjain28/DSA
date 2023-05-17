4921f1e762e2a8068ad59eb2c34a6d410ff3859a
Aryan Jain <aryanjn807@gmail.com>
2023-05-17 11:05:17 -0400

    added heaps

---
+++ heaps.py
@@ -0,0 +1,14 @@
+
+# 1. Structure Property
+# values are added left to right
+# complete bin tree.
+
+# 2. Order Property.
+# root is always the minimum (or maximum)
+# every value in the left and in the right 
+#   subtree will be greater than the root.
+
+# We can also have duplicates in heaps.
+
+
+
