3d823336e982115c6779a22bfc003047206b3250
Aryan Jain <aryanjn807@gmail.com>
2023-05-18 10:01:25 -0400

    added heapify helper

--- heaps.py
+++ heaps.py
@@ -24,25 +24,34 @@ class Heap:
             self.heap[index // 2] = self.heap[index // 2], self.heap[index]
             index = index // 2
 
+    def heapify(self):
+
+        index = 1
+        LEN = len(self.heap)
+        while True:
+            # check if it has right child, meaning it has both child?
+            if index*2+1 < LEN:
+                # check for minimum
+                if  self.heap[index*2] < self.heap[index*2+1] and self.heap[index*2] < self.heap[index]:
+                    self.heap[index*2], self.heap[index] = self.heap[index], self.heap[index*2]
+                    index *= 2
+                elif self.heap[index*2] > self.heap[index*2+1] and self.heap[index*2+1] < self.heap[index]:
+                    self.heap[index*2+1], self.heap[index] = self.heap[index], self.heap[index*2+1]
+                    index = index * 2 + 1
+                else:
+                    break
+            # check if it has left child?
+            elif index*2 < LEN and self.heap[index] > self.heap[index*2]:
+                self.heap[index*2], self.heap[index] = self.heap[index], self.heap[index*2]
+                index *= 2
+            else:
+                break
+
     def pop(self) -> int:
 
         self.heap[1], self.heap[-1] = self.heap[-1], self.heap[1]
         popval = self.heap.pop()
-
-        index = 1
-        while self.heap[index] > self.heap[index * 2] or self.heap[index] > self.heap[(index * 2) + 1]:
-
-            if self.heap[index * 2] > self.heap[index * 2 + 1]:
-                index2 = index * 2 + 1
-            elif self.heap[index * 2] < self.heap[index * 2 + 1]:
-                index2 = index * 2
-
-            self.heap[index], self.heap[index2] = self.heap[index2], self.heap[index]
-            index = index2
-
-            if index * 2 >= len(self.heap):
-                break
-
+        self.heapify()
         return popval
 
 
