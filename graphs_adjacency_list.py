6c835d84f00b126d0b12c827337b358ff595efda
Aryan Jain <aryanjn807@gmail.com>
2023-05-31 07:56:51 -0400

    added def using list

--- graphs_adjacency_list.py
+++ graphs_adjacency_list.py
@@ -10,3 +10,25 @@ for nodeTo, nodeFrom in edges:
 
 
 print(adjList)
+
+
+visit = set()
+
+
+def dfs(src, dest):
+
+    if src in visit:
+        return 0
+    if src == dest:
+        return 1
+
+    count = 0
+    visit.add(src)
+    for nodeTo in adjList.get(src, []):
+        count += dfs(nodeTo, dest)
+    visit.remove(src)
+
+    return count
+
+
+print(dfs("A", "E"))
