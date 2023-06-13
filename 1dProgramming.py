d9ad74932c3f66eeed2f1b4ea8b5d6fc89546ffb
Aryan Jain <aryanjn807@gmail.com>
2023-06-13 08:26:30 -0400

    -> added more on 1d

--- 1dProgramming.py
+++ 1dProgramming.py
@@ -17,7 +17,7 @@ def bruteForce(n):
 
 # Memoization
 
-
+# Top Down Approach
 def memoization(n, cache):
 
     if n <= 1:
@@ -35,4 +35,23 @@ def memoization(n, cache):
 # print(memoization(2, {}))
 # print(memoization(3, {}))
 # print(memoization(9, {}))
-print(memoization(10, {}))
+# print(memoization(10, {}))
+
+
+# Bottom Up Approach: True DP
+
+def dp(n):
+
+    fn1, fn2 = 0, 1
+    for _ in range(2, n+1):
+        fn1, fn2 = fn2, fn1 + fn2
+
+    return fn2
+
+
+print(dp(1))
+print(dp(2))
+print(dp(3))
+print(dp(4))
+print(dp(5))
+print(dp(6))
