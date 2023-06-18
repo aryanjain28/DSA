deb54b450083717fb15d5a5d1c5d3c62cada2eb6
Aryan Jain <aryanjn807@gmail.com>
2023-06-18 13:48:58 -0400

    -> added 2d bottom up

--- 2dProgramming.py
+++ 2dProgramming.py
@@ -34,11 +34,11 @@ n = 10
 
 def countPaths2(row: int = 0, col: int = 0) -> int:
 
-    if (row, col) in cache:
-        return cache[(row, col)]
-
     if row >= n or col >= n:
         return 0
+
+    if (row, col) in cache:
+        return cache[(row, col)]
 
     if row == n - 1 and col == n - 1:
         return 1
@@ -53,3 +53,28 @@ diff2 = time() - start_time
 print("# ways: ", res, " Time taken with cache: ", diff2)
 
 print(abs(diff1 - diff2))
+
+
+# Bottom Up approach
+
+n = 4
+m = 4
+
+
+def countPaths3():
+
+    # init
+    r1 = [0] * m
+    r1[-1] = 1
+
+    for _ in range(n):
+        r2 = [1] * m
+        for index in range(m-2, -1, -1):
+            r2[index] = r1[index] + r2[index + 1]
+
+        r1 = r2
+
+    return r2[0]
+
+
+print(countPaths3())
