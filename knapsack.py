1ca292a00627b909e7497853f01ad41fa1087a54
Aryan Jain <aryanjn807@gmail.com>
2023-06-19 09:28:24 -0400

    -> knapsack using bottom up

--- knapsack.py
+++ knapsack.py
@@ -49,11 +49,35 @@ def knapSack(profit: list[int], weight: list[int], capacity: int) -> int:
 
     for item in range(1, N):
         for cap in range(1, M+1):
-            included = profit[item] + matrix[item-1][cap-weight[item]] if cap >= weight[item] else 0
+            included = profit[item] + matrix[item-1][cap -
+                                                     weight[item]] if cap >= weight[item] else 0
             matrix[item][cap] = max(matrix[item-1][cap], included)
 
     print(matrix)
     return matrix[-1][-1]
+
+
+knapSack([4, 4, 7, 1], [5, 2, 3, 1], 8)
+
+
+# True dp - using less memory
+
+def knapSack(profit: list[int], weight: list[int], capacity: int) -> int:
+
+    N = len(profit)
+    M = capacity
+
+    r1 = [profit[0] if c >= weight[0] else 0 for c in range(M+1)]
+
+    for i in range(1, N):
+        r2 = [0] * (M + 1)
+        for cap in range(1, M+1):
+            included = profit[i] + r1[cap-weight[i]] if cap >= weight[i] else 0
+            r2[cap] = max(r1[cap], included)
+        r1 = r2
+
+    print(r2)
+    return r2[-1]
 
 
 knapSack([4, 4, 7, 1], [5, 2, 3, 1], 8)
