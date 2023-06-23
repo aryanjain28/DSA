c8f78af0f66f50a171b31c366b911a96521d2f33
Aryan Jain <aryanjn807@gmail.com>
2023-06-23 09:08:04 -0400

    update knapsack_unbounded.py

--- knapsack_unbounded.py
+++ knapsack_unbounded.py
@@ -29,4 +29,35 @@ def knapsack_unbounded(profits: list[int], weights: list[int], capacity: int) ->
     return res
 
 
-knapsack_unbounded([4, 4, 7, 1], [5, 2, 3, 1], 8)
+# knapsack_unbounded([4, 4, 7, 1], [5, 2, 3, 1], 8)
+
+
+# using loop recursively
+def knapsack_unbounded_loop(profits: list[int], weights: list[int], capacity: int) -> int:
+
+    LEN = len(profits)
+
+    cache = {}  # Memoization
+
+    def rec(index: int, currCap: int) -> int:
+
+        if index == LEN:
+            return 0
+
+        if (index, currCap) in cache:
+            return cache[index, currCap]
+
+        maxProfit = -1
+        for i, wt in enumerate(weights):
+            if currCap >= wt:
+                maxProfit = max(maxProfit, profits[index] + rec(i, currCap-wt))
+
+        cache[(index, currCap)] = maxProfit
+        return cache[(index, currCap)]
+
+    res = rec(0, capacity)
+    print("MAX: ", res)
+    return res
+
+
+knapsack_unbounded_loop([4, 4, 7, 1], [5, 2, 3, 1], 8)
