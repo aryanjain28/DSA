47d06875deac77277ece47990811f7ba75f46f05
Aryan Jain <aryanjn807@gmail.com>
2023-06-23 09:32:08 -0400

    update knapsack_unbounded.py : added Tabulation

--- knapsack_unbounded.py
+++ knapsack_unbounded.py
@@ -32,6 +32,30 @@ def knapsack_unbounded(profits: list[int], weights: list[int], capacity: int) ->
 # knapsack_unbounded([4, 4, 7, 1], [5, 2, 3, 1], 8)
 
 
+# True Dynamic Solution
+def knapsack_unbounded_tabulation(profits: list[int], weights: list[int], capacity: int) -> int:
+
+    M = len(profits)  # rows
+    N = capacity  # columns
+    T = [[0] * (capacity + 1) for _ in range(M+1)]
+
+    for index in range(1, M+1):
+        for currCap in range(1, N+1):
+            exclude = T[index-1][currCap]
+            currWt = weights[index-1]
+            if currCap >= currWt:
+                include = profits[index-1] + T[index][currCap-currWt]
+                T[index][currCap] = max(include, exclude)
+            else:
+                T[index][currCap] = exclude
+
+    print(T)
+    return T[-1][-1]
+
+
+knapsack_unbounded_tabulation([4, 4, 7, 1], [5, 2, 3, 1], 8)
+
+
 # using loop recursively
 def knapsack_unbounded_loop(profits: list[int], weights: list[int], capacity: int) -> int:
 
@@ -60,4 +84,4 @@ def knapsack_unbounded_loop(profits: list[int], weights: list[int], capacity: in
     return res
 
 
-knapsack_unbounded_loop([4, 4, 7, 1], [5, 2, 3, 1], 8)
+# knapsack_unbounded_loop([4, 4, 7, 1], [5, 2, 3, 1], 8)
