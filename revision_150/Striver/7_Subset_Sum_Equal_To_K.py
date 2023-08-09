8017a303bb62a857e67a5c6b0ef5f37f2e09e55a
Aryan Jain <aryanjn807@gmail.com>
2023-08-09 16:19:19 -0400

    update 7_Subset_Sum_Equal_To_K.py

--- revision_150/Striver/7_Subset_Sum_Equal_To_K.py
+++ revision_150/Striver/7_Subset_Sum_Equal_To_K.py
@@ -13,29 +13,39 @@ def subsetSumToK(n, k, arr):
         if (index, target) in dp:
             return dp[(index, target)]
 
-        if target == 0:
-            return True
-
         if index < 0 or target < 0:
             return False
 
-        # not select
-        right = rec(index-1, target)
-        # select
-        left = False
-        if target >= arr[index]:
-            left = rec(index-1, target-arr[index])
+        if target == 0:
+            return True
 
-        dp[(index, target)] = left or right
+        nSelect = rec(index-1, target)
+        select = False
+        if target-arr[index] >= 0:
+            select = rec(index-1, target-arr[index])
+
+        dp[(index, target)] = select or nSelect
         return dp[(index, target)]
 
-    return rec(n-1, k)
+    # return rec(n-1, k)
 
+    # taulation
+    dp = [[False for _ in range(k+2)] for _ in range(n+1)]
+    for i in range(len(dp)):
+        dp[i][1] = True
 
-arr = [int(n.strip())
-       for n in "8 59 58 79 44 7 65 69 60 51".split(" ")]
+    for i in range(1, n+1):
+        for target in range(2, k+2):
+
+            nSelect = dp[i-1][target]
+            select = False
+            if target-arr[i-1] >= 0:
+                select = dp[i-1][target-arr[i-1]]
+
+            dp[i][target] = select or nSelect
+
+    return dp[-1][-1]
 
 
 print(subsetSumToK(4, 4, [6, 1, 2, 1]))
-# print(subsetSumToK(4, 4, [1, 2, 3, 4]))
-print(subsetSumToK(len(arr), 172, arr))
+print(subsetSumToK(3, 4, [6, 1, 2]))
