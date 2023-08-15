9abaad062a97870e2da16d4a9d410e6e5fd2288a
Aryan Jain <aryanjn807@gmail.com>
2023-08-15 14:15:43 -0400

    feat: create 300.longest-increasing-subsequence.py and 368.largest-divisible-subset.py

---
+++ revision_150/300.longest-increasing-subsequence.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=300 lang=python
+#
+# [300] Longest Increasing Subsequence
+#
+
+# @lc code=start
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        
+
+        
+# @lc code=end
+
+print(Solution().lengthOfLIS([10,9,2,5,3,7,101,18]))
+print(Solution().lengthOfLIS([0,1,0,2,3]))
+print(Solution().lengthOfLIS([0,1,0,3,2,3]))
+print(Solution().lengthOfLIS([7,7,7,7,7,7,7,7,7]))
