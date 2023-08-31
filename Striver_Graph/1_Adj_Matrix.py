995318e23f563171b39ad1ac92b27d70193767aa
Aryan Jain <aryanjn807@gmail.com>
2023-08-31 10:21:33 -0400

    update 1_Adj_Matrix.py

--- Striver_Graph/1_Adj_Matrix.py
+++ Striver_Graph/1_Adj_Matrix.py
@@ -11,12 +11,25 @@ nodes = [
     [4, 5],
 ]
 
+# Adj Matrix
 matrix = [[0] * (n+1) for _ in range(n+1)]
 
 for n1, n2 in nodes:
     matrix[n1][n2] = 1
     matrix[n2][n1] = 1
 
-for m in matrix:
-    print(m)
+# for m in matrix:
+#     print(m)
 
+
+# Adj List
+adjList = {}
+for n1, n2 in nodes:
+    adjList[n1] = adjList.get(n1, set())
+    adjList[n2] = adjList.get(n2, set())
+
+    adjList[n1].add(n2)
+    adjList[n2].add(n1)
+
+
+print(adjList)
