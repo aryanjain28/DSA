f280fa89f4ccf157fde7e11f4e3e08431ae12250
Aryan Jain <aryanjn807@gmail.com>
2023-09-14 08:41:39 -0400

    feat: create _Dijkstra_Algorithm_PQ.py

---
+++ Striver_Graph/_Dijkstra_Algorithm_PQ.py
@@ -0,0 +1,42 @@
+
+
+import heapq 
+
+class Solution:
+    
+    def dijkstra(self, nodes, adj, src):
+
+        adjList = {}
+        for parent, neighbors in enumerate(adj):
+            for node, wt in neighbors:
+                if parent not in adjList:
+                    adjList[parent] = []
+
+                adjList[parent].append((node, wt))
+
+
+        distance = [float("inf")] * nodes
+        distance[src] = 0
+
+        heap = [(0, src)]
+
+        while heap:
+            d, node = heapq.heappop(heap)
+            
+            for (neighbor, wt) in adjList[node]:
+                if distance[neighbor] > d + wt:
+                    distance[neighbor] = d + wt
+                    heapq.heappush(heap, (distance[neighbor], neighbor))
+            
+        
+        return distance
+        
+
+
+
+
+
+
+adj = [[(1, 1), (2, 6)], [(2, 3), (0, 1)], [(1, 3), (0, 6)]]
+
+Solution().dijkstra(3, adj, 2)
