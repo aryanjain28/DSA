6564b77a64a6c3b785eebefacdbd7e172ac6f38e
aryanjain28 <aryanjn807@gmail.com>
2023-10-25 16:27:59 -0400

    feat: create 40.combination-sum-ii.py create

---
+++ Striver_150_SDE/40.combination-sum-ii.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=40 lang=python
+#
+# [40] Combination Sum II
+#
+
+# @lc code=start
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+
+    
+        
+# @lc code=end
+
