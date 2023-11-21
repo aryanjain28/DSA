8275017b16fd6013d3e78c4bc08d9dee89539c6c
aryanjain28 <aryanjn807@gmail.com>
2023-11-21 16:53:45 -0500

    update 40.combination-sum-ii.py

--- Striver_150_SDE/40.combination-sum-ii.py
+++ Striver_150_SDE/40.combination-sum-ii.py
@@ -13,7 +13,43 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
+        if sum(candidates) < target:
+            return []
+        elif sum(candidates) < target:
+            return candidates
+
+        candidates.sort()
+        n = len(candidates)
+        res = []
+
+        dp = {}
+        def dfs(index, s, arr):
+
+            if s == target:
+                res.append(arr)
+                return
+
+            if s > target or index >= n:
+                return 
+            
+            dfs(index + 1, s + candidates[index], arr + [candidates[index]])
+            
+            # not select
+            index += 1
+            while index < n and candidates[index] == candidates[index-1]:
+                index += 1
+            
+            
+            dfs(index, s, arr)
+            
+
+        dfs(0, 0, [])
+        return set([tuple(element) for element in res])
+
+        
+
     
         
 # @lc code=end
-
+print(Solution().combinationSum2([3,1,2], 3))
+print(Solution().combinationSum2([10,1,2,7,6,1,5], target = 8))
